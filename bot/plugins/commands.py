86b6e2063dda371ba90c33e576c865f6a0f61d93
Arshadkn <93204996+Arshadkn@users.noreply.github.com>
2021-10-28 12:24:04 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f",{file_name} /n @Movievillagegroup"‚
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developer', url="https://t.me/sabotschannel"
+                                    '🎖️join main🎖️, url="https://t.me/Movievillagegroup"
                                 )
                         ]
                     ]
